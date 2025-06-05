f0cb7139b68eaef29472ac1ae5b3ca47ee4bc272
Isa <ninjamandalov@gmail.com>
2025-06-05 18:46:17 +0100

    Single static table for Goblin LootTable - Should be done for all future classes to avoid excessive duplicate classes

--- src/main/java/knaefelda/enemies/Goblin.java
+++ src/main/java/knaefelda/enemies/Goblin.java
@@ -7,15 +7,17 @@ import knaefelda.Pair;
 
 public class Goblin extends Enemy {
 
+    private static LootTable goblinLootTable = new LootTable(new HashMap<>() {{
+        put("Bone", new Pair<>(0.5, 1));
+        put("Gold", new Pair<>(0.2, 1));
+    }});
+
     public Goblin() {
         super(
             "Goblin",
             10,
             2,
-            new LootTable(new HashMap<>() {{
-                put("Bone", new Pair<>(0.5, 1));
-                put("Gold", new Pair<>(0.2, 1));
-            }}),
+            goblinLootTable,
             2);
     }
 
